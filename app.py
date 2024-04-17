6d1a2a2f92db4378d4f86c411d0f80cbcafbc93a
sawsent <vicenteppfigueiredo@gmail.com>
2024-04-18 00:54:54 +0100

    mvp api v6

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from service import playlistService
 import jsonFormatter
 
+
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/api/get/<message>')
 def hello_world(message):
