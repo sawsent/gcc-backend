cfa0bfed5a956bf7179909e9e7748aa0f91e838d
sawsent <vicenteppfigueiredo@gmail.com>
2024-04-18 11:54:28 +0100

    gradients api done

--- gradientFetcher.py
+++ gradientFetcher.py
@@ -4,7 +4,7 @@ import os
 
 class GradientFetcher:
     def __init__(self) -> None:
-        openai.api_key = os.getenv('OPENAI_API_KEY')
+        openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
     def sendRequest(self, message):
@@ -12,8 +12,8 @@ class GradientFetcher:
         response = openai.ChatCompletion.create(
             model='gpt-3.5-turbo',
             messages=[
-                {"role": "system", "content": "You are an assistent that gives me a beautiful css gradient background with 4 edges. It needs to mirror my mood in a way that will make me feel better. You give it in this format: [background: linear-gradient(45deg, #83A8A6, #497A78, #C0CDC4, #83A8A6)]"},
-                {"role": "user", "content": message},
+                {"role": "system", "content": "You are an assistent that gives me a beautiful css gradient background with 4 edges. It needs to mirror my mood in a way that will make me feel better. You give it in this format: [background: linear-gradient(45deg, #83A8A6, #497A78, #C0CDC4, #83A8A6)] even if you dont understand what im telling you, generate a css gradient background."},
+                {"role": "user", "content": "Im feeling " + message},
             ],
             temperature=1.3
         )
@@ -21,7 +21,6 @@ class GradientFetcher:
     
     
     def find_content_in_brackets(self, text):
-        print(text)
         matches = re.findall(r"\[(.*?)\]", text)
         if matches:
             return matches[0]
@@ -36,6 +35,6 @@ class GradientFetcher:
         gradient = self.find_content_in_brackets(nelson_response)
 
         return {
-            'gradient': gradient,
+            'gradient': gradient + ';',
         }
 
